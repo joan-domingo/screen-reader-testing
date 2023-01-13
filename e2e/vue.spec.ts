f949e1d000e0a7f1d6d37a78f1c07c39074d7805
Joan Domingo <joan.domingo@dkbcodefactory.com>
2023-01-13 16:57:33 +0100

    update gitlab ci

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test';
 // See here how to get started:
 // https://playwright.dev/docs/intro
 test('visits the app root url', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
+  await page.goto('https://vuejs.org/');
+  await expect(page.locator('p.description')).toHaveText('An approachable, performant and versatile framework for building web user interfaces.');
 })
